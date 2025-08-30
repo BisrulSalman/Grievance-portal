11ba71044ffe8c4687095b904e3d4eeac12eadcb
BisrulSalman <salmanbisrul@gmail.com>
2025-08-30 12:44:19 +0530

    Delete Grievance-Portal-main/Grievance-Portal-main directory

--- Grievance-Portal-main/Grievance-Portal-main/backend/routes/admin_request.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-const express = require("express");
-const router = express.Router();
-
-router.get("/register", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../pages/admin-registration.html"));
-});
-
-module.exports = router;
